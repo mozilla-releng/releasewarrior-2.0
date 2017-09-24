7210f39456564db28ad440d9f526281fc306df28
Jordan Lund <jlund@mozilla.com>
2017-09-23 17:22:26 -0700

    adds create pipeline cli command

---
+++ releasewarrior/cli.py
@@ -0,0 +1,23 @@
+import click
+
+@click.group()
+def cli():
+    """Releasewarrior: helping you keep track of releases in flight
+
+    Each sub command takes a product and version
+
+    versioning:\n
+    \tBetas: must have a 'b' within string\n
+    \tRelease Candidates: must have a 'rc' within string\n
+    \tESRs: must have an 'esr' within string\n
+    """
+    pass
+
+@cli.command()
+@click.argument('product', type=click.Choice(['firefox', 'devedition', 'fennec', 'thunderbird']))
+@click.argument('version')
+@click.option('--date', help="date of planned GTB. format: YYYY-MM-DD")
+def create(product, version, date):
+    """start tracking an upcoming release.
+    """
+    click.echo('creating release pipeline for {} {}'.format(product, version))
